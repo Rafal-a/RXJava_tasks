457484b70920275cebceb02180abca371a2ebbbb
Rafal A <rafal.akeel99@gmail.com>
2021-09-19 01:10:29 +0300

    altering the function that contains errors

--- app/src/main/java/com/ibareq/rxjavatasks/RXJavaTasks.kt
+++ app/src/main/java/com/ibareq/rxjavatasks/RXJavaTasks.kt
@@ -17,7 +17,10 @@ object RXJavaTasks {
      * let it emit characters form A to Z each 1 second
      */
     fun task1(): Observable<String> {
-        return Observable.
+        val listLetters = listOf<String>("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
+            ,"P","Q","R","S","T","U","V","W","X","Y","Z")
+        return Observable.fromIterable(listLetters)
+            .zipWith(Observable.interval(1, TimeUnit.SECONDS), {item, _ -> item})
     }
 
     /**
@@ -26,7 +29,7 @@ object RXJavaTasks {
      */
     fun task2(): Observable<String> {
         val mList = listOf("A", "B", "C", "C", "D", "B", "E")
-        return Observable.fromIterable(mList)
+        return Observable.fromIterable(mList).distinct()
             .zipWith(Observable.interval(300, TimeUnit.MILLISECONDS), {item, _ -> item})
     }
 
@@ -37,7 +40,7 @@ object RXJavaTasks {
     fun task3(): Observable<String> {
         val firstObservable = Observable.just("A", "B", "C", "D", "E")
         val secondObservable = Observable.range(1,5)
-        return firstObservable.mergeWith(secondObservable)
+        return firstObservable.mergeWith(secondObservable.map {number -> number.toString() })
             .zipWith(Observable.interval(300, TimeUnit.MILLISECONDS), {item, _ -> item})
     }
 
@@ -46,7 +49,7 @@ object RXJavaTasks {
      * let it emit characters form A to Z each 1 second
      */
     fun task4(): Observable<Int> {
-        return Observable.range(1,100)
+        return Observable.range(20,60)
             .zipWith(Observable.interval(300, TimeUnit.MILLISECONDS), {item, _ -> item})
     }
 
@@ -57,7 +60,9 @@ object RXJavaTasks {
         val firstObservable = Observable.just("A", "B", "C", "D", "E").zipWith(Observable.interval(300, TimeUnit.MILLISECONDS), {item, _ -> item})
         val secondObservable = Observable.range(1,5).zipWith(Observable.interval(300, TimeUnit.MILLISECONDS), {item, _ -> item})
 
-        return Observable.
+        return firstObservable.zipWith(secondObservable,{
+                letter:String , number:Int -> "$letter$number"
+        })
     }
 
 }
